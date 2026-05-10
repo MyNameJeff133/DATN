bebbd261d205717fbe443a080ea0d4951b87fd35
MyNameJeff113 <hdila152k4@gmail.com>
2026-05-10 18:18:24 +0700

    update 101

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
+dotenv.config();
 import app from "./app.js";
 import connectDB from "./config/database.js";
 
-dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 
 connectDB();
